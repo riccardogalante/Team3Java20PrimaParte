515fa27f399cae8a1d1355555c9e740206578aef
Rykard <rickygalante@hotmail.it>
2023-11-22 16:05:28 +0100

    aggiornamento Riccardo

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Riccardo, Galante, 29, Palermo, powerlift, carbonara," +
+                " Elden Ring,  Piccoli Brividi, Kingsman, Cipo");
+    }
+}
+
