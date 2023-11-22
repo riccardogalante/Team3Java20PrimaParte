8fc8e1bdf86a40253cf45e95d6928ee41f7179df
DENISE.DL <denise.dl@hotmail.it>
2023-11-22 16:28:59 +0100

    Add a new_System.out.println

--- Main.java
+++ Main.java
@@ -2,6 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Riccardo, Galante, 29, Palermo, powerlift, carbonara," +
                 " Elden Ring,  Piccoli Brividi, Kingsman, Cipo");
+
+        System.out.println("Denise, Denise, 28, Cosenza, Pilates, Tutto," +
+                " Vuoto ,  Orgoglio e pregiudizio, Mrs. Doubtfire, Vuoto");
+
     }
 }
+
+
+
 
