8d9310e30c5e25936be80cb00852ca6ab297d9da
DENISE.DL <denise.dl@hotmail.it>
2023-11-29 16:17:08 +0100

    messo metodo per prendere l'età

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,17 +1,24 @@
+package src;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Riccardo, Galante, 29, Palermo, powerlift, carbonara," +
-                " Elden Ring,  Piccoli Brividi, Kingsman, Cipo");
-
-        System.out.println("Denise, De Leo, 28, Cosenza, Pilates, Tutto," +
-                " Vuoto ,  Orgoglio e pregiudizio, Mrs. Doubtfire, Vuoto");
 
 
-        System.out.println("Roberto, Favaro, 19, Zoppola, Videogiochi, Pizza," +
-                " Halo ,  Senza domani, Nessuno, Luna");
+        Studente studente1 = new Studente("Riccardo", "Galante",29, "Palermo" ,
+                "powerlift" , " carbonara" , "Elden Ring" , "Piccoli Brividi" , "Kingsman", " Cipo");
 
-        System.out.println("Marc Josef, Conanan, 23, Milano, Nuoto, cotoletta bolognese," +
-                " Dota2 ,  Norwegian Wood, Pulp fiction, Ciccio");
+        Studente studente2 = new Studente("Denise", "De Leo",28, "Cosenza" ,
+                "Pilates" , " Tutto" , " " , "Mrs. Doubtfire" , "Orgoglio e pregiudizio", "  ");
+
+        Studente studente3 = new Studente("Roberto", "Favaro",19, "Zoppola" ,
+                "Videogiochi" , " Pizza" , "Halo" , "Senza domani" , "Nessuno", "Luna");
+
+        Studente studente4 = new Studente("Marc Josef", "Conanan",23, "Milano" ,
+                "Nuoto" , "cotoletta bolognese" , " Dota2" , " Pulp fiction" , " Norwegian Wood", "Ciccio");
+
+
 
 
     }
