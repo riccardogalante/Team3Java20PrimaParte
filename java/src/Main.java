5ff09f638f7029d6a7e475c90610e77dac6be96d
Marc Josef Conanan <coanananmarc@gmail.com>
2023-11-29 18:51:23 +0100

    x

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,7 +1,4 @@
 package src;
-
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         // Creating instances of Studente
@@ -9,29 +6,17 @@ public class Main {
                 "powerlift", " carbonara", "Elden Ring", "Piccoli Brividi", "Kingsman", " Cipo");
 
         Studente studente2 = new Studente("Denise", "De Leo", 28, "Cosenza",
-                "Pilates", " Tutto", "", "Mrs. Doubtfire", "Orgoglio e pregiudizio", "");
+                "Pilates", " Tutto", " ", "Mrs. Doubtfire", "Orgoglio e pregiudizio", "  ");
 
         Studente studente3 = new Studente("Roberto", "Favaro", 19, "Zoppola",
-                "Videogiochi", " Pizza", "Halo", "Senza domani", "", "Luna");
+                "Videogiochi", " Pizza", "Halo", "Senza domani", "Nessuno", "Luna");
 
         Studente studente4 = new Studente("Marc Josef", "Conanan", 23, "Milano",
                 "Nuoto", "cotoletta bolognese", " Dota2", " Pulp fiction", " Norwegian Wood", "Ciccio");
 
         Studente[] team = {studente1, studente2, studente3, studente4};
 
-        ordineAlfabetico(studente1.getCognome()+ " "+ studente1.getNome(), studente2.getCognome()+ " " + studente2.getNome(), studente3.getCognome()+ " " + studente3.getNome(), studente4.getCognome()+ " "+ studente4.getNome());
-
         datoEtà(team);
-
-        stampaCucciolo(team);
-
-        videogiocoLOL(team);
-    }
-    //metodo per l'ordine alfabetico
-    public static void ordineAlfabetico(String studente1, String studente2, String studente3, String studente4){
-        String[] cognome = {String.valueOf(studente1), String.valueOf(studente2), String.valueOf(studente3), String.valueOf(studente4)};
-        Arrays.sort(cognome);
-        System.out.println("Squadra in ordine alfabetico : " + Arrays.toString(cognome));
     }
     // metodo per prendere l'età
     public static void datoEtà(Studente[] team) {
@@ -47,22 +32,6 @@ public class Main {
         System.out.println("Squadra in ordine di età:");
         for (int i = 0; i < team.length; i++) {
             System.out.println(team[i].getNome() + " " + team[i].getEtà());
-        }
-    }
-    public static void stampaCucciolo(Studente[] team){
-        for(int i = 0; i < team.length; i++){
-            if(!team[i].getNomeCucciolo().isEmpty()){
-                System.out.println("Studenti con animale domestico : " + team[i].getNome());
-            }
-        }
-    }
-    public static void videogiocoLOL(Studente[] team){
-        for(int i = 0; i < team.length; i++){
-            if(team[i].getVideogiocoPreferito() == "LOL"){
-                System.out.println("Ha scritto LOL come videgioco preferito: "+ team[i].getNome());
-            }else if (team[i].getVideogiocoPreferito() == "League of legend"){
-                System.out.println("Ha scritto League of legend come videogioco preferito: " + team[i].getNome());
-            }
         }
     }
 }
