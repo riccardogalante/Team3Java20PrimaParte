1cf3d232b85e8ad18284f99456666069c454e5b6
RobertoFavaro <robyfavaro04@gmail.com>
2023-11-29 16:19:32 +0100

    collegato il maina alla cartella

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,5 +1,8 @@
+package src;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Riccardo, Galante, 29, Palermo, powerlift, carbonara," +
                 " Elden Ring,  Piccoli Brividi, Kingsman, Cipo");
 
